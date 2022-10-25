26caf1abc027fe53fd2121b447bf28ca1216d658
zbob889 <zroberts889@gmail.com>
2022-10-25 16:15:24 +0200

    Set up

---
+++ express-locallibrary-tutorial/routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+// display the text "You're so cool" at URL /users/cool/
+router.get('/cool/', function(req, res, next) {
+  res.send("You're so cool");
+});
+
+
+module.exports = router;
